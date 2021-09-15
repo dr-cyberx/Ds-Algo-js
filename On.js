2f3cd0dfd152d13e7aac2c42ac6d7c53f7c9157c
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-09-15 13:20:26 +0530

    intro to 0(n)

---
+++ On.js
@@ -0,0 +1,20 @@
+
+const {performance} = require('perf_hooks');
+// const arr = ['nemo'];
+// const secondArr = ['ram', 'sham', 'jhone', 'saurav', 'deepak', 'alex', 'raman', 'nemo']
+const arr = new Array(10000000).fill('nemo')
+
+
+function findNemo(arr) {
+  let t0 = performance.now();
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i]=== 'nemo'){
+      // console.log('nemo found')
+    }
+  }
+  let t1 = performance.now();
+
+  console.log('function took : ', t1 - t0 + " miliseconds")
+}
+
+findNemo(arr) //O(n) => linear time
