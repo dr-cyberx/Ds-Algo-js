e00ae3a0fdaded33f938fe5900115d5ec43f5352
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-09-15 17:03:40 +0530

    changes

---
+++ Exercise1.js
@@ -0,0 +1,13 @@
+function funChallenge(input) {
+  let a = 10;
+  a = 50 + 3;
+
+  for (let i = 0; i < input.length; i++) {
+    anotherFunction();
+    let stranger = true;
+    a++;
+  }
+  return a;
+}
+
+funChallenge(someInput);
