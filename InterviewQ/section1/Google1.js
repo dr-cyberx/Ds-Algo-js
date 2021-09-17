d78481330059db4aaf552c77de13a2be629e134f
ankushcodedrill <ankush.codedrill@gmail.com>
2021-09-17 13:18:38 +0530

    solving interview question with normal way and ideal way

---
+++ InterviewQ/section1/Google1.js
@@ -0,0 +1,20 @@
+const arr1 = ["a", "b", "c", "d"];
+const arr2 = ["x", "y", "z", "m"];
+const arr3 = ["m", "n", "o", "a"];
+
+function findSameVal(array1, array2) {
+    let success;
+  array1.forEach((element) => {
+    array2.forEach((element1) => {
+      if (element === element1) {
+        success = true
+      }else{
+        success = false
+      }
+    });
+  });
+
+  return success;
+}
+
+console.log(findSameVal(arr1, arr2));
