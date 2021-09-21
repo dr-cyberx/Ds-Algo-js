6a844075b89a3e4dcc540a1a388dae2118c037d4
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-09-21 12:31:19 +0530

    addings extract keys

--- Intro/hashTable/creatingHash.js
+++ Intro/hashTable/creatingHash.js
@@ -31,13 +31,32 @@ class hashTable {
       return undefined;
     }
   }
+
+  keys() {
+    let result = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] && this.data[i].length) {
+        console.log(this.data[i].length);
+
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0]);
+          }
+        } else {
+          result.push(this.data[i][0]);
+        }
+      }
+    }
+    return result;
+  }
 }
 
 let hash = new hashTable(10);
 // console.log(hash._hash("vishal"));
 hash.set("name", "vishal");
+hash.set("name", "Sourav");
 hash.set("company", "codedrill");
 hash.set("age", "45");
 // console.log(hash.set("position", "web dev"));
-console.log(hash.get("age"));
+console.log(hash.keys());
 // console.log(hash)
